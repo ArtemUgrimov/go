17edd69b9a07e456e0e4436421d14b94b21da3cb
Artem <1@1.com>
2024-03-10 22:36:51 +0100

    HW3

---
+++ l3/entities.go
@@ -0,0 +1,85 @@
+package main
+
+import "fmt"
+
+type Stage struct {
+	promptDescription string
+	enterDescription  string
+	decisions         []*Stage
+}
+
+type Player struct {
+	name          string
+	stage         *Stage
+	hp            int
+	isBleeding    bool
+	extraStrength int
+	warmth        int
+}
+
+func (player *Player) spawn() {
+	fmt.Printf("Як тебе звати, друже? \n")
+	fmt.Scanf("%s", &player.name)
+	player.stage = nil
+	player.hp = 100
+	player.isBleeding = false
+	player.extraStrength = 0
+	player.warmth = 100
+}
+
+func (player *Player) updateState() {
+	if player.isBleeding {
+		player.hp -= 30
+	} else {
+		player.hp -= 10
+	}
+	if player.extraStrength >= 50 {
+		player.warmth += 20
+	}
+	player.extraStrength += 10
+}
+
+func (player *Player) printState() {
+	if player.stage != nil {
+		fmt.Println(player.stage.enterDescription)
+		if player.hp < 50 {
+			fmt.Printf("%s відчуває різкий біль по всьому тілу, рана від укусу дається взнаки, а в очах періодично темніє\n", player.name)
+		}
+		if player.isBleeding {
+			fmt.Println("Кров постійно тече з рани")
+		}
+		if player.extraStrength > 30 && player.extraStrength < 70 {
+			fmt.Printf("%s відчуває незрозумілий прилив сил, гілки в лісі стали майже пір'ям, а бігати вдається удвічі швидше\n", player.name)
+		} else if player.extraStrength > 70 {
+			fmt.Printf("%s може наздогнати будь-яку тварину в лісі, а стрибнути так, що іноді влається спіймати птаха\n", player.name)
+		}
+		if player.warmth < 50 {
+			fmt.Println("{{name}} відчуває сильний холод")
+		}
+	} else {
+		fmt.Printf("%s в лісі, непритомний.\n", player.name)
+	}
+}
+
+func (stage *Stage) prompt(player *Player) (res int) {
+	fmt.Println(stage.enterDescription)
+
+	var valuesCount int = 0
+	for _, descision := range stage.decisions {
+		if descision != nil {
+			fmt.Println(descision.promptDescription)
+			valuesCount++
+		}
+	}
+
+	if valuesCount > 0 {
+		for res < 1 || res > valuesCount {
+			fmt.Printf("Зроби вибір (від 1 до %d)\n", valuesCount)
+			fmt.Scan(&res)
+			if res < 1 || res > valuesCount {
+				player.printState()
+			}
+		}
+	}
+	return
+}
--- l3/main.go
+++ l3/main.go
@@ -1,77 +1,34 @@
 package main
 
-func countTwoCharacters(input string, char1, char2 rune) (firstCount int32, secondCount int32) {
-	for _, val := range input {
-		switch val {
-		case char1:
-			firstCount++
-		case char2:
-			secondCount++
-		}
-	}
-	return
-}
+import (
+	"fmt"
+)
+
+/*
+   Можливий сценарій: Стівен прокинувся біля входу в печеру.
+
+*/
 
 func main() {
-	/*
-	   Можливий сценарій: Стівен прокинувся біля входу в печеру.
-	   Він лише памʼятає своє імʼя. Поряд з ним рюкзак, в якому він знаходить сірники, ліхтарик і ніж.
-	   У печері темно, тому Стівен іде стежкою, яка веде від печери в ліс.
-	   У лісі Стівен натикається на мертве тіло дивної тварини. Він обирає нічого з цим не робити й іти далі.
-	   Через деякий час Стівен приходить до безлюдного табору.
-	   Він вже втомлений і вирішує відпочити, а не йти далі.
-	   У найближчому наметі він знаходить сейф з кодовим замком з двох чисел.
-	   Він добирає код, і коли сейф відчиняється, йому на долоню виповзає велика комаха, кусає його й тікає.
-	   Стівен непритомніє. А все могло бути зовсім інакше.
-	*/
+	//i am using a pointer to not to write & while passing the player as an argument
+	var player *Player = &Player{}
+	player.spawn()
 
-	// char1, char2 := 'a', 'c'
-	// text := "abcd abc cd"
-	// count1, count2 := countTwoCharacters(text, char1, char2)
-	// fmt.Printf("character `%c` occured %d times, character `%c` occured %d times\n", char1, count1, char2, count2)
+	start := stage1(player)
+	fmt.Println(start.promptDescription)
+	playerChoice := start.prompt(player)
 
-	// N := 30
-	// K := 10
-	// for i := 1; i < N; i++ {
-	// 	if i%K == 0 {
-	// 		fmt.Println(i)
-	// 	}
-	// }
+	step2 := stage2(player, start, playerChoice)
+	playerChoice = step2.prompt(player)
 
-	// a := []int{10, 20, 333, 0, -50}
-	// for idx, elem := range a {
-	// 	fmt.Println(idx, elem)
-	// }
+	step3 := stage3(player, step2, playerChoice)
+	playerChoice = step3.prompt(player)
 
-	// for _, elem := range a {
-	// fmt.Println(elem)
-	// }
+	step4 := stage4(player, step3, playerChoice)
+	step4.prompt(player)
 
-	/*
-		name := "Artem"
-		nameLen := len(name)
+	finalStage := final(player, step4)
+	finalStage.prompt(player)
 
-		switch nameLen {
-		case 5:
-			fmt.Println("Artem's name length is 5")
-		case 6:
-			fmt.Println("???")
-		default:
-			fmt.Println("Something went wrong")
-		}
-
-		for i := 0; i < 100; i++ {
-			rnd := rand.Int() % 10
-			if rnd == 5 {
-				fmt.Printf("Rnd = %d. Break\n", rnd)
-				break
-			} else if rnd == 7 {
-				continue
-			}
-
-			fmt.Printf("%dth iteation\n", i)
-		}
-
-		for pos, char
-	*/
+	player.printState()
 }
